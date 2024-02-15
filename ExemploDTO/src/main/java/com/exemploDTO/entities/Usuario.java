ce41c44f2b7a84f7d05b4879a33b3543e5e59279
isazy <146368490+isazy@users.noreply.github.com>
2024-02-15 14:29:49 -0300

    Add files via upload

---
+++ ExemploDTO/src/main/java/com/exemploDTO/entities/Usuario.java
@@ -0,0 +1,72 @@
+package com.exemploDTO.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@NotBlank
+	private String nome;
+
+	@NotNull
+	@NotBlank
+	private String senha;
+
+	private String permissao;
+
+	
+
+	public Usuario(@NotNull @NotBlank String nome, @NotNull @NotBlank String senha) {
+		this.nome = nome;
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getPermissao() {
+		return permissao;
+	}
+
+	public void setPermissao(String permissao) {
+		this.permissao = permissao;
+	}
+	
+	
+
+
+}
